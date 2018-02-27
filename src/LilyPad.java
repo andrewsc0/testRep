3e5513f0dbd640d3acc4727e6169e6e2efe025b5
CHRISAN <CHRISAN@IT_BK99X52.mshome.net>
2018-02-27 11:42:18 -0500

    Second push

--- src/LilyPad.java
+++ src/LilyPad.java
@@ -4,6 +4,9 @@ public class LilyPad {
 	int numHops = 0;
 	int curr = 0;
 	
+	
+	// calculates how many hops the frog has to take
+	// to get the the final lilyPad
 	public int howManyHops(int numLilys, int canHop) {
 		
 		while(curr<numLilys) {
@@ -29,6 +32,11 @@ public class LilyPad {
 	public int hoeManyHopsOp(int numLilys, int canHop) {
 		
 		return (int) Math.ceil(numLilys/canHop);
+		
+	}
+	
+	public void getNum() {
+		
 		
 	}
 
